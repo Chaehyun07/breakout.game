c82b113c67738b3b66a0ccb03319b9b7f4870f06
Chaehyun07 <shooin1025@gmail.com>
2024-07-22 11:41:29 +0900

    패들 움직임

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ pygame.init()
 #컬러코드 rgb
 bbeige_marke = (250, 237, 201)
 ford_dark_charcoal = (50, 50, 50)
+black = (0, 0, 0)
 
 #화면 크기 설정
 screen_width = 800
@@ -18,6 +19,14 @@ ball_y =  screen_height //2 - ball_height // 2
 ball_dx = 3
 ball_dy = -3
 
+#패드 설정
+paddle_width = 100
+paddle_height = 10
+paddle_x = screen_width // 2 - paddle_width // 2
+paddle_y = screen_height - 40
+paddle_dx = 0
+paddle_speed = 6
+
 screen = pygame.display.set_mode((screen_width,screen_height))
 #게임 표시 제목
 pygame.display.set_caption("벽돌 깨기 게임")
@@ -29,10 +38,28 @@ clock = pygame.time.Clock()
 
 
 while running:
+    #이벤트 처리
     for event in pygame.event.get():
-        if event in  pygame.event.get() :
-         if event.type == pygame.Quit :
-            running = False
+        if event.type == pygame.QUIT :
+           running = False
+            #키가 눌렸을 때 발생
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                paddle_dx = -paddle_speed
+            if event.key == pygame.K_RIGHT:
+                paddle_dx = paddle_speed
+           #키가 떼어졌을 때 발생
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                paddle_dx = 0
+
+#패들 이동
+    paddle_x += paddle_dx
+    if paddle_x < 0:
+        paddle_x = 0
+    if paddle_x > screen_width - paddle_width:
+        paddle_x = screen_width - paddle_width
+
 
     ball_x += ball_dx
     ball_y += ball_dy
@@ -49,6 +76,10 @@ while running:
 
     pygame.draw.ellipse(screen, ford_dark_charcoal,(ball_x, ball_y, ball_width, ball_height)
                         )
+    #패들 그리기
+
+    paddle_rect = pygame.Rect (paddle_x, paddle_y, paddle_width, paddle_height)
+    pygame.draw.rect(screen, black, paddle_rect)
 
     pygame.display.flip()
     clock.tick(60)
